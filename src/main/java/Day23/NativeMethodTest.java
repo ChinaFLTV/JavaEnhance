d90319846ec0768a90adfef2ebf3a652aa60ff59
ChinaFLTV <236010069@qq.com>
2022-10-23 15:56:39 +0800

    2022-10-23  15:56-Day23

---
+++ src/main/java/Day23/NativeMethodTest.java
@@ -0,0 +1,21 @@
+package Day23;
+
+/*
+ ** @author LiGuanda
+ ** @date  PM 3:10:48
+ ** @description This is a description of NativeMethodTest
+ ** @params
+ ** @return
+ ** @since version-1.0  */
+
+
+/**
+ * @author Lenovo
+ */
+public class NativeMethodTest {
+
+
+    public native void love();
+
+
+}
