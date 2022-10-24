8ebb3a383461770b965cfa11ae0b754d4049206b
ChinaFLTV <236010069@qq.com>
2022-10-24 18:57:10 +0800

    2022-10-24  18:57-Day24

---
+++ src/main/java/Day24/JarTest.java
@@ -0,0 +1,46 @@
+package Day24;
+
+import static java.lang.System.out;
+
+/**
+ * @author LiGuanda
+ * @date PM 6:36:55
+ * @description This is a description of JarTest
+ * @see Day24.JarTest#love(int)
+ * @since version-1.0
+ */
+
+
+public class JarTest {
+
+
+    /**
+     * dage 's wife
+     */
+    public String wife = "IU";
+
+
+    public static void main(String[] args) {
+
+        out.println("Hello World");
+
+    }
+
+
+    /**
+     * <em>heiheihei</em>
+     * @description hhhhhhhhhhhhh
+     * @param dage hehe
+     * @return do
+     * @index heihei
+     * @see <a href="https://www.google.com">谷歌</a>
+     * @see "IU loves dage"
+     */
+    public double love(int dage) {
+
+        return 6.6;
+
+    }
+
+
+}
